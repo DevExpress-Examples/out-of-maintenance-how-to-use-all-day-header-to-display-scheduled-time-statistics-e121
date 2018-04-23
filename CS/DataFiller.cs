b9a8afc45e2822993a35b2cbf16049fcaef01984
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DataFiller.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DevExpress.XtraScheduler;
+
+namespace DayHeaderStatistics {
+    public class DataFiller {
+        public static string[] Users = new string[] { "Sarah Brighton", "Ryan Fischer", "Andrew Miller" };
+        public static string[] Usernames = new string[] { "sbrighton", "rfischer", "amiller" };
+        public static Random RandomInstance = new Random();
+
+
+        public static void FillResources(SchedulerStorage storage, int count) {
+            ResourceCollection resources = storage.Resources.Items;
+            storage.BeginUpdate();
+            try {
+                int cnt = Math.Min(count, Users.Length);
+                for (int i = 1; i <= cnt; i++) {
+                    resources.Add(new Resource(Usernames[i - 1], Users[i - 1]));
+                }
+            }
+            finally {
+                storage.EndUpdate();
+            }
+        }
+
+
+        public static void GenerateAppointments(SchedulerStorage storage) {
+            int count = storage.Resources.Count;
+            for (int i = 0; i < count; i++) {
+                Resource resource = storage.Resources[i];
+                string subjPrefix = resource.Caption + "'s ";
+
+                storage.Appointments.Add (AptCreate(resource.Id, subjPrefix + "meeting", 2, 5));
+                storage.Appointments.Add (AptCreate(resource.Id, subjPrefix + "travel", 3, 6));
+                storage.Appointments.Add (AptCreate(resource.Id, subjPrefix + "phone call", 0, 10));
+            }
+        }
+        public static Appointment AptCreate(object resourceId, string subject, int status, int label) {
+            Appointment apt = new Appointment();
+            apt.Subject = subject;
+            apt.ResourceId = resourceId;
+            Random rnd = RandomInstance;
+            int rangeInHours = 48;
+            apt.Start = DateTime.Today + TimeSpan.FromHours(rnd.Next(0, rangeInHours));
+            apt.End = apt.Start + TimeSpan.FromHours(rnd.Next(0, rangeInHours / 8));
+            apt.StatusId = status;
+            apt.LabelId = label;
+            return apt;
+        }
+
+    }
+}
---
+++ VB/Form1.vb
@@ -0,0 +1,62 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Linq
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraScheduler.Drawing
+Imports DevExpress.XtraScheduler
+Imports DevExpress.XtraScheduler.Native
+Imports DevExpress.XtraScheduler.Tools
+
+Namespace DayHeaderStatistics
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            DataFiller.FillResources(schedulerControl1.Storage, 2)
+            DataFiller.GenerateAppointments(schedulerControl1.Storage)
+            schedulerControl1.GroupType = SchedulerGroupType.Resource
+            schedulerControl1.Start = Date.Now
+        End Sub
+
+        Private Sub schedulerControl1_CustomDrawDayViewAllDayArea(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.CustomDrawObjectEventArgs) Handles schedulerControl1.CustomDrawDayViewAllDayArea
+            If schedulerStorage1.Appointments IsNot Nothing Then
+                Dim cell As AllDayAreaCell = CType(e.ObjectInfo, AllDayAreaCell)
+                Dim resource As Resource = cell.Resource
+                Dim interval As TimeInterval = cell.Interval
+
+                Dim percent As Single = CalcCurrentWorkTimeLoad(interval, resource)
+                Dim brush As Brush
+                If percent = 0.0 Then
+                    brush = Brushes.LightYellow
+                ElseIf percent < 0.5 Then
+                    brush = Brushes.LightBlue
+                Else
+                    brush = Brushes.LightCoral
+                End If
+                e.Cache.FillRectangle(brush, e.Bounds)
+                e.Cache.DrawString(String.Format("{0:P}", percent), cell.Appearance.Font, Brushes.Black, e.Bounds, cell.Appearance.TextOptions.GetStringFormat())
+                e.Handled = True
+            End If
+        End Sub
+
+        Private Function CalcCurrentWorkTimeLoad(ByVal interval As TimeInterval, ByVal resource As Resource) As Single
+            Dim apts As AppointmentBaseCollection = Me.schedulerStorage1.GetAppointments(interval)
+            Dim aptsByResource As New AppointmentBaseCollection()
+			Dim aptQuery = apts.Where(Function(a) a.ResourceId.Equals(resource.Id))
+            aptsByResource.AddRange(aptQuery.ToList())
+
+            Dim calc As New IntervalLoadRatioCalculator(interval, aptsByResource)
+            Return calc.Calculate()
+        End Function
+    End Class
+
+
+End Namespace
